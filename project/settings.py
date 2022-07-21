9b1939de7d9ceb6b7ce8a6286047362e7bb237a2
Deepak Singh <ds1990singh@yahoo.com>
2022-07-21 08:03:39 +0530

    django-rest-api-testing-deep changed

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!zxmc^48#eli@!494w9^#56#nf*xp7s&#zu+n)82z&yxsnwnu#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['quiet-brook-50483.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['quiet-brook-50483.herokuapp.com','localhost','127.0.0.1','django-rest-api-testing-deep.herokuapp.com']
 
 
 
